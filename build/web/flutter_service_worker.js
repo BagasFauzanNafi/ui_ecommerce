5fec661a1329b68d8eec32d00f326da542e06f1b
unknown <bagasnafi19@gmail.com>
2024-12-09 14:18:50 +0700

    update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -93,7 +93,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "23ee180bd3c529a97f6d751f29b8dbf7
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "02c61a072598ba407a3a48ee563533dd",
+"flutter_bootstrap.js": "dc7bb5cfc50ef398af5870d5836c128c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
